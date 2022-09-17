3593d9bc99d563c5fbefdc46fc00623d76ff1133
hiroto0222 <aoyamahiroto0222@gmail.com>
2022-09-17 17:02:18 +0900

    create initial tests for palindrome check true/false

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+class PalindromeChecker {
+  isPalindrome(str: string): boolean {
+    const reversed = str.split('').reverse().join('');
+    return str === reversed;
+  }
+}
+
+export default PalindromeChecker;
